bd489e4928fb7119bc7bbd48bfd7ba13e4e3ab4b
sobidmardi2025-prog <sobidmardi2025@gmail.com>
2026-06-24 17:16:32 +0530

    Initialize Supabase client with URL and key

--- supabase.js
+++ supabase.js
@@ -1,1 +1,8 @@
+const SUPABASE_URL = "https://kenfvhsvrnvfsuawawmq.supabase.co";
 
+const SUPABASE_KEY = "sb_publishable_D-_HWvvpxfIVFz4G5Ru6Xw_aaC2ay1l";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
